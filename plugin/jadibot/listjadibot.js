fec865a374822e8e64b19679a8bc1a9c6036cf5f
Shizu-Hub <112402377+Shizu-Hub@users.noreply.github.com>
2022-09-01 17:16:02 +0100

    Add files via upload

---
+++ plugin/jadibot/listjadibot.js
@@ -0,0 +1,24 @@
+const prettyms = require('pretty-ms')
+
+module.exports = {
+  name: "listjadibot",
+  alias: ["listjadibot","listbot"],
+  category: "jadibot",
+  desc: "mostrando a los usuarios que se suben a los bots!!",
+  async run({conn, msg}){
+    try {
+      let user = [... new Set([...global.conns.filter(conn => conn).map(conn => conn)])]
+      te = "*Lista sub bots*\n\n"
+      te += ` Total : ${global.conns.length}\n\n`
+      for(let i of user){
+        y = await conn.decodeJid(i.user.id)
+        te += " × Usuarios : @" + y.split("@")[0] + "\n"
+        te += " × Nombre : " + i.user.name + "\n"
+        te += " × activo desde : " + await prettyms(Date.now() - i.time, { verbose: true, }) + "\n\n"
+      }
+      user != "" ? await msg.reply(te,{withTag : true}) : await msg.reply("_*No hay usuarios que sean sub-bots..*_")
+    } catch (e){
+      global.error(msg.command, e, msg)
+    }
+  }
+}
